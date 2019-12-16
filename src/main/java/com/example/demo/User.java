999c588b9e827b15f86adf3821700a1beae8e1e7
Abhishek Singhal <abhishek.singhal7795@gmail.com>
2019-12-16 23:39:47 +0530

    Added Identifier

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity(name = "users")
 public class User {
-
+	@Id
 	private String id;
 	private String name;
 
