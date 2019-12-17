52ee111ba77f04b5f6b8d4a13448aeef67cf2778
Abhishek Singhal <abhishek.singhal7795@gmail.com>
2019-12-17 12:16:22 +0530

    Removed startup db entry

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,24 +1,14 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application implements CommandLineRunner {
-
-	@Autowired
-	private UserRepository userRepository;
+public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		userRepository.save(new User("1", "Abhishek Singhal"));
-		userRepository.save(new User("2", "Test Name"));
 	}
 
 }
