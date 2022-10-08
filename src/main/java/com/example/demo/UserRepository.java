759f4f703bf8b0b06906fcb0d21ea77cf9b01b36
Abhishek Singhal <abhishek.singhal7795@gmail.com>
2022-10-09 00:33:37 +0530

    spring boot version update

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
